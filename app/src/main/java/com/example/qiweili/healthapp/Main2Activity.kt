433af01f55f424078f25c7c91bc1efb79e940176
liqiwei <liqiwei@iastate.edu>
2018-01-22 14:09:27 -0600

    Added

--- app/src/main/java/com/example/qiweili/healthapp/Main2Activity.kt
+++ app/src/main/java/com/example/qiweili/healthapp/Main2Activity.kt
@@ -2,11 +2,21 @@ package com.example.qiweili.healthapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+        val mGoogleSignInClient : GoogleSignInClient;
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
     }
 }
