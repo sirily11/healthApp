6204f2c5438c70ec863a8893df2beabe7ce9f0eb
Lucas Golinghorst <golinlg@iastate.edu>
2018-02-20 08:08:27 -0600

    added in my activities to android manifest made a back button in the edit meal class

--- app/src/main/java/com/example/qiweili/healthapp/EditMeal.java
+++ app/src/main/java/com/example/qiweili/healthapp/EditMeal.java
@@ -1,8 +1,11 @@
 package com.example.qiweili.healthapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class EditMeal extends AppCompatActivity {
 
@@ -12,5 +15,18 @@ public class EditMeal extends AppCompatActivity {
         setContentView(R.layout.activity_edit_meal);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button back = findViewById(R.id.backButton);
+
+        back.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                goBack(v);
+            }
+        });
+    }
+
+    public void goBack(View v) {
+        Intent intent = new Intent(this, MyMeals.class);
+        startActivity(intent);
     }
 }
